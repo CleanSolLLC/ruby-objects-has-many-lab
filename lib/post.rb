89da1146a1cbc311117cefb79860c334881e3933
CleanSolLLC <mamark90@gmail.com>
2019-09-19 21:00:10 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class Post
+
+	attr_accessor :author, :title, :name
+
+	@@all = []
+
+	def initialize(title)
+		@title = title
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def author_name
+		if self.author != nil
+			self.author.name
+		elsif self.author = nil
+			nil
+		end
+				
+	end
+end
